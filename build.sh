86588b3ef9dd8a978ad3b533ea0732736d2a66df
Adrien LAUER <adrien.lauer@mpsa.com>
2016-02-17 10:30:33 +0100

    Fail whole build when a step fails

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "Building frontend..."
-(cd frontend && npm install && grunt $TASK)
+(cd frontend && npm install && grunt $TASK) || exit 1
 
 echo "Copying frontend resources..."
-mkdir -p backend/src/main/resources/META-INF/resources
-cp -R frontend/dist/* backend/src/main/resources/META-INF/resources
+mkdir -p backend/src/main/resources/META-INF/resources || exit 1
+cp -R frontend/dist/* backend/src/main/resources/META-INF/resources || exit 1
 
 echo "Building backend..."
-(cd backend && mvn -q -P$ADDITIONAL_PROFILES ${GOAL:-clean install} jacoco:report)
+(cd backend && mvn -q -P$ADDITIONAL_PROFILES ${GOAL:-clean install} jacoco:report) || exit 1
 
 echo "Done."
