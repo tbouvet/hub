899e88f0d982134c7e95a271b3a96b81ae158c25
Adrien LAUER <adrien.lauer@mpsa.com>
2016-02-26 12:16:01 +0100

    Fix travis variable export

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "Building frontend..."
+echo "Building frontend (tasks: $TASK)..."
 (cd frontend && npm install && gulp $TASK) || exit 1
 
 echo "Copying frontend resources..."
 mkdir -p backend/src/main/resources/META-INF/resources || exit 1
 cp -R frontend/dist/* backend/src/main/resources/META-INF/resources || exit 1
 
-echo "Building backend..."
+echo "Building backend (profiles: $ADDITIONAL_PROFILES, goals: $GOAL)..."
 (cd backend && mvn -q -P$ADDITIONAL_PROFILES ${GOAL:-clean install} jacoco:report) || exit 1
 
 echo "Done."
