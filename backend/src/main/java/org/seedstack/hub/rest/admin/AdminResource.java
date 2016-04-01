4f27bf98bc85c774b777a425de752117357a909c
pith <pierre.thirouin@ext.mpsa.com>
2016-04-01 17:00:04 +0200

    Parallel import

--- backend/src/main/java/org/seedstack/hub/rest/admin/AdminResource.java
+++ backend/src/main/java/org/seedstack/hub/rest/admin/AdminResource.java
@@ -8,9 +8,14 @@
 package org.seedstack.hub.rest.admin;
 
 import org.seedstack.hub.application.fetch.ImportService;
+import org.seedstack.hub.domain.model.component.Component;
 import org.seedstack.hub.domain.model.component.Source;
+import org.seedstack.hub.rest.component.list.ComponentCard;
+import org.seedstack.hub.rest.component.list.ComponentCardAssembler;
+import org.seedstack.seed.Logging;
 import org.seedstack.seed.rest.Rel;
 import org.seedstack.seed.security.RequiresRoles;
+import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -18,7 +23,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.seedstack.hub.rest.Rels.IMPORT_LIST;
 
@@ -26,16 +33,33 @@ import static org.seedstack.hub.rest.Rels.IMPORT_LIST;
 @Produces({"application/json", "application/hal+json"})
 public class AdminResource {
 
+    @Logging
+    private Logger logger;
     @Inject
     private ImportService importService;
+    @Inject
+    private ComponentCardAssembler assembler;
 
-    @Rel(value = IMPORT_LIST, home = true)
     @POST
     @RequiresRoles("admin")
     @Path("/import")
+    @Rel(value = IMPORT_LIST, home = true)
     @Consumes({"application/json"})
     public Response importList(List<Source> sources) {
-        sources.forEach(importService::importComponent);
-        return Response.ok().build();
+        long startTime = System.currentTimeMillis();
+
+        List<ComponentCard> componentCards = sources.parallelStream()
+                .map(source -> {
+                    logger.info("importing " + source.getUrl());
+                    Component component = importService.importComponent(source);
+                    logger.info("imported " + source.getUrl());
+                    return component;
+                })
+                .map(assembler::assembleDtoFromAggregate)
+                .collect(Collectors.toList());
+
+        long stopTime = System.currentTimeMillis();
+        logger.info(Duration.ofMillis(stopTime - startTime).toString());
+        return Response.ok().entity(componentCards).build();
     }
 }
